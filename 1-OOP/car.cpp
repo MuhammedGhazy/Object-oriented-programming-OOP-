99832a981cb512f58b9ff6bf13aacecb421fd99a
Muhammed Ghazy <ghazymuhammed00@gmail.com>
2024-02-15 04:00:12 +0200

    OOP

---
+++ 1-OOP/car.cpp
@@ -0,0 +1,17 @@
+#include "car.h"
+void car::Set_Maker(string m)
+{
+    maker = m;
+}
+string car::Get_Maker()
+{
+    return (maker);
+}
+void car::Set_Model(int m)
+{
+    model = m;
+}
+int car::Get_Model()
+{
+    return (model);
+}
---
+++ 1-OOP/car.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+class car
+{
+private:
+    string maker;
+    int model;
+    string color;
+
+public:
+    void Set_Maker(string m);
+    std:: string Get_Maker();
+    void Set_Model(int m);
+    int Get_Model();
+};
+
